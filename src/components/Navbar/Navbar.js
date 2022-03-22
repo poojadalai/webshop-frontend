feee0e7642fb92cf7b88545297e57938cd7e9055
poojadalai <poojadalai@gmail.com>
2022-03-22 10:46:08 +0100

    added navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,31 +1,73 @@
 import React from "react";
 import "./styles.css";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHeart,
+  faSearch,
+  faShoppingCart,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar(props) {
   return (
-    <nav className="nav">
-      <ul className="navUL">
-        <li className="navLi logo">
-          <a href="/">
-            The<span className="shop">Shop</span>
-          </a>
-        </li>
-        <li className="navLi">
-          <a href="/">Home</a>
-        </li>
-        <li className="navLi">
-          <a href="/">Products</a>
-        </li>
-        {/* <li className="navLi">
-          <a href="/">Blog</a>
-        </li> */}
-        <li className="navLi">
-          <a href="/shop">Shop</a>
-        </li>
-        {/* <li className="navLi">
-          <a href="/">Contacts</a>
-        </li> */}
-      </ul>
-    </nav>
+    <div className="menu">
+      <nav className="nav">
+        <ul className="nav-ul">
+          <li className="nav-li logo">
+            <NavLink to="/shop" className="nav-li">
+              The<span className="shop">Shop</span>
+            </NavLink>
+          </li>
+          <li className="nav-li">
+            <NavLink
+              to="/"
+              className="nav-li"
+              style={({ isActive }) =>
+                isActive ? { color: "white" } : undefined
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-li">
+            <NavLink
+              to="/shop"
+              className="nav-li"
+              style={({ isActive }) =>
+                isActive ? { color: "white" } : undefined
+              }
+            >
+              Products
+            </NavLink>
+          </li>
+          <li className="nav-li">
+            <NavLink
+              to="/details"
+              className="nav-li"
+              style={({ isActive }) =>
+                isActive ? { color: "white" } : undefined
+              }
+            >
+              Shop
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <form>
+        <input className="add" type="search" placeholder="search here..."></input>
+      </form>
+      <div className="shop-links">
+        <NavLink to="/" className="icons">
+          <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+        </NavLink>
+        <NavLink to="/" className="icons">
+          <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+        </NavLink>
+        <NavLink to="/" className="icons">
+          <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
+        </NavLink>
+      </div>
+    </div>
   );
 }
