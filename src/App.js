3d8b782a06ac138a9650385d5036a60576cf3c0b
poojadalai <poojadalai@gmail.com>
2022-03-24 13:51:04 +0100

    functional signup page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import { Routes, Route } from "react-router-dom";
 import { Footer, Navbar } from "./components";
 import { Details, Home, Shop, Signup } from "./Pages";
 import { Login } from "./Pages/Login";
+import { useEffect } from "react";
 
 function App() {
+
+  // const [token, setToken] = useState("")
+  // const [user, setUser] = useState()
+
+  // useEffect(() => {
+  //   if (token){
+  //     // fetch user info from backend
+  //   }
+  // }, [token])
+
+
   return (
     <div className="App">
+       {/* <Navbar user={user}/> */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
